295115554a63fb648d7995e5757960d871ef90c9
saad-1761 <sadbin1761@gmail.com>
2025-12-19 05:23:01 +0600

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Products from "../../components/Home/Products";
+
+const Home = () => {
+  return (
+    <div>
+      <Products />
+      {/* More components */}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,128 @@
+import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import ProductDetails from "../pages/ProductDetails/ProductDetails";
+import PrivateRoute from "./PrivateRoute";
+import DashboardLayout from "../layouts/DashboardLayout";
+import AddProduct from "../pages/Dashboard/Seller/AddProduct";
+import ManageUsers from "../pages/Dashboard/Admin/ManageUsers";
+import Profile from "../pages/Dashboard/Common/Profile";
+import Statistics from "../pages/Dashboard/Common/Statistics";
+import MainLayout from "../layouts/MainLayout";
+import MyInventory from "../pages/Dashboard/Seller/MyInventory";
+import ManageOrders from "../pages/Dashboard/Seller/ManageOrders";
+import MyOrders from "../pages/Dashboard/Customer/MyOrders";
+import { createBrowserRouter } from "react-router";
+import PaymentSuccess from "../pages/Payment/PaymentSuccess";
+import SellerRequests from "../pages/Dashboard/Admin/SellerRequests";
+import SellerRoute from "./SellerRoute";
+import AdminRoute from "./AdminRoute";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetails />,
+      },
+      {
+        path: "/payment-success",
+        element: <PaymentSuccess />,
+      },
+    ],
+  },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <SignUp /> },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <PrivateRoute>
+            <Statistics />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "add-product",
+        element: (
+          <PrivateRoute>
+            <SellerRoute>
+              <AddProduct />
+            </SellerRoute>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "my-inventory",
+        element: (
+          <PrivateRoute>
+            <SellerRoute>
+              <MyInventory />
+            </SellerRoute>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "manage-users",
+        element: (
+          <PrivateRoute>
+            <AdminRoute>
+              <ManageUsers />
+            </AdminRoute>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "seller-requests",
+        element: (
+          <PrivateRoute>
+            <AdminRoute>
+              <SellerRequests />
+            </AdminRoute>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "my-orders",
+        element: (
+          <PrivateRoute>
+            <MyOrders />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "manage-orders",
+        element: (
+          <PrivateRoute>
+            <SellerRoute>
+              <ManageOrders />
+            </SellerRoute>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
